0e7cddea996ac9bc0055532af27f806d9bc5094c
Nikita Koynov <nikicha03@gmail.com>
2020-04-14 00:33:23 +0300

    Added Spring config. Closes #22

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/PetServiceMap.java
@@ -2,7 +2,9 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Pet;
 import com.example.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetServiceMap.java
@@ -2,7 +2,9 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
