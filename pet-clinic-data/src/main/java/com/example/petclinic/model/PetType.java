b3cc65a1b0de5dcdfe80393c9b2badd6af42e5f3
Nikita Koynov <nikicha03@gmail.com>
2020-04-21 16:24:46 +0300

    Visit added, Pet and PetType relations modified. Closes #26

--- pet-clinic-data/src/main/java/com/example/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/PetType.java
@@ -1,8 +1,11 @@
 package com.example.petclinic.model;
 
+import java.util.Set;
+
 public class PetType extends BaseEntity{
 
     private String name;
+    private Set<Pet> pets;
 
     public String getName() {
         return this.name;
@@ -10,5 +13,13 @@ public class PetType extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
