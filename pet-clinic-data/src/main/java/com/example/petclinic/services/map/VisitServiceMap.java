7c524f612c4e8ade071619e3e08e20cab328026c
Nikita Koynov <nikicha03@gmail.com>
2020-04-30 22:48:44 +0300

    Visit Map Service added. Closes #46

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,17 @@
+package com.example.petclinic.services.map;
+
+import com.example.petclinic.model.Visit;
+import com.example.petclinic.services.VisitService;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit!");
+        }
+
+        return super.save(visit);
+    }
+}
